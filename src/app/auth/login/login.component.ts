99bb3779f7cc11d7bb34215c0caa777f89e89ae5
maurya20 <mukesh.ice17@gmail.com>
2021-11-19 15:39:20 +0530

    Login form added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  submitForm(form: NgForm): void {
+    console.log('?????form', form);
   }
-
 }
