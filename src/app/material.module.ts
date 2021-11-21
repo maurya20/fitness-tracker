a4f706bdd582cbd565986c8da7562bbb89b85aa7
maurya20 <mukesh.ice17@gmail.com>
2021-11-21 16:03:14 +0530

    mat sort implemeted

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -15,6 +15,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 @NgModule({
   imports: [
     MatButtonModule,
@@ -32,6 +33,7 @@ import { MatTableModule } from '@angular/material/table';
     MatProgressSpinnerModule,
     MatDialogModule,
     MatTableModule,
+    MatSortModule,
   ],
   exports: [
     MatButtonModule,
@@ -50,6 +52,7 @@ import { MatTableModule } from '@angular/material/table';
     MatProgressSpinnerModule,
     MatDialogModule,
     MatTableModule,
+    MatSortModule,
   ],
 })
 export class MaterialModule {}
--- src/app/training/past/past.component.ts
+++ src/app/training/past/past.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { IExercise } from 'src/app/models/exercise.model';
 import { ExerciseService } from 'src/app/services/exercise.service';
@@ -8,13 +9,23 @@ import { ExerciseService } from 'src/app/services/exercise.service';
   templateUrl: './past.component.html',
   styleUrls: ['./past.component.scss'],
 })
-export class PastComponent implements OnInit {
-  displayedColumns = ['date', 'name', 'duration', 'caloriesBurned', 'state'];
-  dataSource = new MatTableDataSource<IExercise>();
+export class PastComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = [
+    'date',
+    'name',
+    'duration',
+    'caloriesBurned',
+    'state',
+  ];
+  dataSource = new MatTableDataSource<IExercise>([]);
+  @ViewChild(MatSort) sort: MatSort;
   constructor(private exerciseService: ExerciseService) {}
 
   ngOnInit(): void {
     this.dataSource.data =
       this.exerciseService.getCompletedOrCancelledTrainings();
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
   }
 }
